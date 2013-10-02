dc17c6129d31ca8e93ee34f88b84d0ab4bb6a295
dnoseda <damian.noseda@mercadolibre.com>
2013-10-02 18:15:14 -0300

    problem with encoding

--- bin/json-to-csv.py
+++ bin/json-to-csv.py
@@ -4,9 +4,59 @@
 # si es mapa, buscar valor que sea arreglo, y hacer ese, o el argumento pasado
 # para cada elemento, si es mapa, recursivamente meterse para generar rows con titulo de clave previa
 
-def myprint(d):
-  for k, v in d.iteritems():
-    if isinstance(v, dict):
-      myprint(v)
-    else:
-      print "{0} : {1}".format(k, v)
+import json
+import sys
+import re
+import csv
+import io
+
+def collect_data(d):
+    for k, v in d.iteritems():
+        if isinstance(v, dict):
+            collect_data(v)
+        else:
+            print "{0} : {1}".format(k, v)
+
+filename = sys.argv[1]
+#fileout = sys.argv[2]
+
+print 'abriendo archivo '+filename
+f = io.open(filename, "r")
+
+text = f.read()
+
+
+json_content = json.loads(text)
+
+to_analize = None
+if isinstance(json_content, dict):
+    for k, v in json_content.iteritems():
+        print "revisando {0}".format(k)
+        if isinstance(v, list):
+            print "uso esto!"
+            to_analize = v
+else:
+    to_analize=json_content
+
+for a in to_analize:
+    collect_data(a)
+
+"""
+columns = dict()
+output_rows = []
+lastrow = 0
+index = {}
+
+collect_data(to_analize, columns, 
+
+for row in to_analize:
+    for k,v in row.items():
+        if not columns.has_key(k):
+            columns[k] = lastrow
+            index[lastrow] = k
+            lastrow = lastrow + 1
+
+
+
+
+"""
